c5de5d40bd5ae9c397799992758508374701bce3
Mahdi Ghaleb <mahdighaleb196@gmail.com>
2024-12-12 20:04:09 -0700

    Save changes before switching branches

---
+++ mockUpQuestions/script.js
@@ -0,0 +1,49 @@
+let currentTimer = null;
+
+const startTimer = () => {
+    let timeLeft = 30;
+    const timerFill = document.querySelector(".timer-fill");
+    const nextButton = document.querySelector(".next-button");
+
+    timerFill.style.animation = "none";
+    setTimeout(() => {
+        timerFill.style.animation = "timer 30s linear forwards";
+    }, 0);
+
+    nextButton.disabled = true;
+
+    currentTimer = setInterval(() => {
+        if (timeLeft > 0) {
+            timeLeft--;
+        } else {
+            console.log("Time up");
+            stopTimer();
+        }
+    }, 1000);
+
+    const stopTimer = () => {
+        console.log("Timer stopped.");
+        clearInterval(currentTimer);
+        timerFill.style.animation = "none"; 
+        currentTimer = null;
+        nextButton.disabled = false;
+    };
+
+    const options = document.querySelectorAll(".option");
+    options.forEach((option) => {
+        option.addEventListener("click", () => {
+            console.log("Option selected!");
+            stopTimer(); 
+        });
+    });
+
+    nextButton.addEventListener("click", () => {
+        console.log("next question");
+        startTimer(); 
+    });
+};
+
+startTimer();
+
+//make it so once picking an option timer countinues to next question
+//fix getting infinte time after slecting an option 
